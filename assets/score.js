1240f83f8d48150c8e345370fde8af0aa45c4bcd
Youssef <youssefkaroui6@gmail.comm>
2022-03-18 14:46:21 -0400

    timer not working

--- assets/score.js
+++ assets/score.js
@@ -1,18 +1,18 @@
 function printHighscores() {
-    // either get scores from localstorage or set to empty array
+    
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
   
-    // sort highscores by score property in descending order
+   
     highscores.sort(function(a, b) {
       return b.score - a.score;
     });
   
     highscores.forEach(function(score) {
-      // create li tag for each high score
+     
       var liTag = document.createElement("li");
       liTag.textContent = score.initials + " - " + score.score;
   
-      // display on page
+      
       var olEl = document.getElementById("highscores");
       olEl.appendChild(liTag);
     });
@@ -25,5 +25,5 @@ function printHighscores() {
   
   document.getElementById("clear").onclick = clearHighscores;
   
-  // run function when page loads
+  
   printHighscores();
