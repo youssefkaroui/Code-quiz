b290615da3eed21ff77253080ff924a1570746a0
Youssef <youssefkaroui6@gmail.comm>
2022-03-18 12:38:19 -0400

    getting-started js

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+// DOM elements
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
+// quiz state variables
+var currentQuestionIndex = 0;
+var time = questions.length * 12;
+var timerId;
+
+function startQuiz() {
+  // hide start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+  // un-hide questions section
+  questionsEl.removeAttribute("class");
+
+  // start timer
+  timerId = setInterval(clockTick, 1000);
+
+  // show starting time
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+
