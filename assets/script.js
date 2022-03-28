333c55c9bc900d3bfa3f62eb0e96e5d6a935ba13
Youssef <youssefkaroui6@gmail.comm>
2022-03-28 16:25:37 -0400

    changes

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,102 @@
 var questionsEl = document.querySelector("#questions");
 var timerEl = document.querySelector("#time");
 var choicesEl = document.querySelector("#choices");
-var submitBtn = document.querySelector("#submit");
+var doItBtn = document.querySelector("#submit");
 var startBtn = document.querySelector("#start");
 var initialsEl = document.querySelector("#initials");
-var feedbackEl = document.querySelector("#feedback");
+var returnMessageEl = document.querySelector("#feedback");
 
+var quizQuestions = [
+   {
+     question: "What are JavaScript Data Types?",
+     choices: ["Number", "String", "Boolean", "Undifined", "All the above"],
+     answer: "All the above"
+   },
+   {
+    question: "which one of these key-words is function scoped?",
+       choices: ["Var", "Let", "Both"],
+       answer: "Var"
+       
+   },
+
+          
+    {
+      question: "Which one is a looping structure in JavaScript?",
+      choices: ["All the below", "For", "While", "do-while loops"],
+      answer: "All the below"
+    },
+    {
+      question: "What are the two basic groups of data types in JavaScript?",
+      choices: [
+        "Primitive and attribute",
+        "Primitive and reference types",
+        "Reference types and attribute",
+        "None of the above"
+      ],
+      answer: "Primitive and reference types"
+    },
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      question: "Boolean operators that can be used in JavaScript include:",
+      choices: [
+        "'And' Operator &&",
+        "'Or' Operator ||",
+        "'Not' Operator !",
+        "All the above"
+      ],
+      answer: "All the above"
+    },
+    
+    {
+      question: "What is the data type of variables in JavaScript?",
+      choices: [
+        "Object data types",
+        "Function data type",
+        "None of the above",
+        "All of the above"
+      ],
+      answer: "Object data types"
+    },
+    {
+      question: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      question:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      question:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    },
+    {
+      question: "What is the type of Pop up boxes available in JavaScript?:",
+      choices: ["Alert", "Confirm", "Prompt", "All the above"],
+      answer: "All the above"
+    }
+  ];
 
 var currentQuestionIndex = 0;
-var time = questions.length * 12;
+var time = quizQuestions.length * 12;
 var timerId;
 
 
@@ -44,11 +132,11 @@ function runTimer() {
 
 function getQuestion() {
  
-  var currentQuestion = questions[currentQuestionIndex];
+  var currentQuestion = quizQuestions[currentQuestionIndex];
 
   
   var titleEl = document.getElementById("question-title");
-  titleEl.textContent = currentQuestion.title;
+  titleEl.textContent = currentQuestion.question;
 
  
   choicesEl.innerHTML = "";
@@ -56,23 +144,23 @@ function getQuestion() {
 
   currentQuestion.choices.forEach(function(choice, i) {
 
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
+    var decision = document.createElement("button");
+    decision.setAttribute("class", "choice");
+    decision.setAttribute("value", choice);
 
-    choiceNode.textContent = i + 1 + ". " + choice;
+    decision.textContent = i + 1 + ". " + choice;
 
 
-    choiceNode.onclick = questionClick;
+    decision.onclick = questionClick;
 
   
-    choicesEl.appendChild(choiceNode);
+    choicesEl.appendChild(decision);
   });
 }
 
 function questionClick() {
 
-  if (this.value !== questions[currentQuestionIndex].answer) {
+  if (this.value !== quizQuestions[currentQuestionIndex].answer) {
 
     time -= 15;
 
@@ -81,26 +169,26 @@ function questionClick() {
     }
    
     timerEl.textContent = time;
-    feedbackEl.textContent = "Wrong";
-    feedbackEl.style.color = "orange";
-    feedbackEl.style.fontSize = "200%";
+    returnMessageEl.textContent = "Wrong";
+    returnMessageEl.style.color = "orange";
+    returnMessageEl.style.fontSize = "200%";
   } else {
-    feedbackEl.textContent = "Correct";
-    feedbackEl.style.color = "blue";
-    feedbackEl.style.fontSize = "200%";
+    returnMessageEl.textContent = "Correct";
+    returnMessageEl.style.color = "blue";
+    returnMessageEl.style.fontSize = "200%";
   }
 
   
-  feedbackEl.setAttribute("class", "feedback");
+  returnMessageEl.setAttribute("class", "feedback");
   setTimeout(function() {
-    feedbackEl.setAttribute("class", "feedback hide");
+    returnMessageEl.setAttribute("class", "feedback hide");
   }, 1000);
 
   
   currentQuestionIndex++;
 
   
-  if (currentQuestionIndex === questions.length) {
+  if (currentQuestionIndex === quizQuestions.length) {
     quizEnd();
   } else {
     getQuestion();
@@ -157,7 +245,7 @@ function checkForEnter(event) {
 }
 
 
-submitBtn.onclick = saveHighscore;
+doItBtn.onclick = saveHighscore;
 
 
 startBtn.onclick = startQuiz;
